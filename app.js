af28dbf919c139982d943e41d788e2c7c43f8825
magpe30 <mpelka892@gmail.com>
2021-07-10 15:14:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+///Validation////
+const submitBtn  = document.getElementById("signup-button");
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("signup-username").value;
+    const email = document.getElementById("signup-email").value;
+    const phoneNumber  = document.getElementById("phonenumber").value;
+    const currentPosition = document.getElementById("position").value;
+    const password = document.getElementById("signup-password").value;
+    const confirmPassword = document.getElementById("password2").value;
+    const findOut = document.getElementById("findout").value;
+    const termsConditions = document.getElementById("terms-conditions");
+    let mistakeParagraph = document.getElementById("wrong-check");
+ 
+    const formInputs = {
+        username: name,
+        email: email,
+        phone: phoneNumber,
+        position: currentPosition,
+        password: password,
+        passwordConfirm: confirmPassword,
+        source: findOut,
+    }
+    
+    let messages  = [];
+    //check all fields
+    if(name == null || name == "", 
+       email == null || email == "", 
+       phoneNumber == null || phoneNumber == "", 
+       currentPosition == null || currentPosition == ""  
+    ){
+        messages.push("All fields are required")
+    }
+    //check if password is at least 6 characters long
+    if (password.length < 6){
+        messages.push("Password should have at least 6 character");
+    }
+    //check if passwords match
+    if(password !== confirmPassword){
+        messages.push("Passwords do not match");
+    }
+    //check if they checked terms and conditions
+    if(!termsConditions.checked){
+        messages.push("Please agree to our Terms and Conditions");
+    }
+    //check if they chose where they know about directly apply
+    if(findOut === "0"){
+        messages.push("Please tell us how do you know about Directly Apply")
+    }
+
+    if(messages.length > 0 ){
+        mistakeParagraph.innerHTML = ""
+        messages.forEach(function(error) {
+            //mistakeParagraph.innerHTML = "";
+            let template = `<p class="alert">${error}</p>`
+            mistakeParagraph.innerHTML += template;
+        })
+    }else{
+        mistakeParagraph.innerHTML = "";
+        let template = `<p class="pass">You are registered !</p>`
+        mistakeParagraph.innerHTML += template;
+        console.log(formInputs);
+    }
+});
